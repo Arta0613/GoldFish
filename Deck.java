61a845e9d5e99c6293757df331b64c66c6433285
Richard <luxh93@gmail.com>
2014-09-21 14:44:06 -0400

    data structures implemented

--- Deck.java
+++ Deck.java
@@ -1,32 +1,79 @@
+import java.util.*;
+
 public class Deck {
-	public Deck() {
-		cards = new Stack<String>();
-		static String[] unshuffledDeck = new String[52];
-		String card;
+	public static void main(String[] args) {
+		Deck deck = new Deck();
 		
-		for(int i = 0; i < 52; i++) 
-			for(int j = 0; j < 13; j++) {
-				card = "";
-				switch(i) {
-					case 0: card = 'A' + Integer.toString(i); break;
-					case 11: card = "Jack" + Integer.toString(i); break;
-					case 12: card "Queen" + Integer.toString(i); break;
-					case 13: card "King" + Integer.toString(i); break;
-					default: card = Integer.toString(j) + Integer.toString(i); break;
+		
+	}
+	public Deck() {
+		deck = new Stack<String>();
+		LinkedList<String> unshuffledDeck = new LinkedList<String>();
+		String card = new String();
+		int randpos;
+		
+		for(int cardnum = 1; cardnum <= 13; cardnum++) 
+			for(int cardtype = 1; cardtype <= 4; cardtype++) {
+				switch(cardnum) {
+					case 1: 
+						switch(cardtype) {
+							case 1: card = "A - Diamond"; break;
+							case 2: card = "A - Clover"; break;
+							case 3: card = "A - Heart"; break;
+							case 4: card = "A - Spade"; break;
+						}
+						break;
+					case 11: 
+						switch(cardtype) {
+							case 1: card = "Jack - Diamond"; break;
+							case 2: card = "Jack - Clover"; break;
+							case 3: card = "Jack - Heart"; break;
+							case 4: card = "Jack - Spade"; break;
+						}
+						break;
+					case 12: 
+						switch(cardtype) {
+							case 1: card = "Queen - Diamond"; break;
+							case 2: card = "Queen - Clover"; break;
+							case 3: card = "Queen - Heart"; break;
+							case 4: card = "Queen - Spade"; break;
+						}
+						break;
+					case 13: 
+						switch(cardtype) {
+							case 1: card = "King - Diamond"; break;
+							case 2: card = "King - Clover"; break;
+							case 3: card = "King - Heart"; break;
+							case 4: card = "King - Spade"; break;
+						}
+						break;
+					default:
+						switch(cardtype) {
+							case 1: card = Integer.toString(cardnum) + " - Diamond"; break;
+							case 2: card = Integer.toString(cardnum) + " - Clover"; break;
+							case 3: card = Integer.toString(cardnum) + " - Heart"; break;
+							case 4: card = Integer.toString(cardnum) + " - Spade"; break;
+						}
+						break;
 				}
 	
-				unshuffledDeck[i] = card;
+				unshuffledDeck.push(card);
 			}
 			
-		for(int i = 0; i < shuffledDeck.length(); i++) { 
-			int randpos = random() % shuffledDeck.length();
-			deck.push(shuffledDeck[randpos]);
+		while(unshuffledDeck.size() != 0) {
+			randpos = (int)(Math.random() * unshuffledDeck.size());
 			
-			shuffledDeck.remove(randompos);
+			deck.push(unshuffledDeck.get(randpos));
+			
+			unshuffledDeck.remove(randpos);
 		}
+
+		for(Iterator<String> iterator = unshuffledDeck.descendingIterator(); iterator.hasNext();) 
+			System.out.println(iterator.next());
 	}
 
-	
+		
+
 	String drawCard() {
 		String draw = deck.peek();
 		deck.pop();
@@ -34,6 +81,5 @@ public class Deck {
 		return draw;
 	}
 	
-	
-	static Stack<String>[] deck;
+	static Stack<String> deck;
 }
