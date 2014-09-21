6435afc2217923f973e43f41b4401859598d0e69
Richard <luxh93@gmail.com>
2014-09-21 16:57:12 -0400

    shortened

--- Deck.java
+++ Deck.java
@@ -4,82 +4,45 @@ public class Deck {
 	public static void main(String[] args) {
 		Deck deck = new Deck();
 		
-		
+		String cardDrawn = new String(deck.drawCard());
+		System.out.println("The card you drew: " + cardDrawn);
 	}
 	public Deck() {
-		deck = new Stack<String>();
 		LinkedList<String> unshuffledDeck = new LinkedList<String>();
-		String card = new String();
+		String shape = new String();
+		String cardDigit = new String();
 		int randpos;
 		
 		for(int cardnum = 1; cardnum <= 13; cardnum++) 
 			for(int cardtype = 1; cardtype <= 4; cardtype++) {
-				switch(cardnum) {
-					case 1: 
-						switch(cardtype) {
-							case 1: card = "A - Diamond"; break;
-							case 2: card = "A - Clover"; break;
-							case 3: card = "A - Heart"; break;
-							case 4: card = "A - Spade"; break;
-						}
-						break;
-					case 11: 
-						switch(cardtype) {
-							case 1: card = "Jack - Diamond"; break;
-							case 2: card = "Jack - Clover"; break;
-							case 3: card = "Jack - Heart"; break;
-							case 4: card = "Jack - Spade"; break;
-						}
-						break;
-					case 12: 
-						switch(cardtype) {
-							case 1: card = "Queen - Diamond"; break;
-							case 2: card = "Queen - Clover"; break;
-							case 3: card = "Queen - Heart"; break;
-							case 4: card = "Queen - Spade"; break;
-						}
-						break;
-					case 13: 
-						switch(cardtype) {
-							case 1: card = "King - Diamond"; break;
-							case 2: card = "King - Clover"; break;
-							case 3: card = "King - Heart"; break;
-							case 4: card = "King - Spade"; break;
-						}
-						break;
-					default:
-						switch(cardtype) {
-							case 1: card = Integer.toString(cardnum) + " - Diamond"; break;
-							case 2: card = Integer.toString(cardnum) + " - Clover"; break;
-							case 3: card = Integer.toString(cardnum) + " - Heart"; break;
-							case 4: card = Integer.toString(cardnum) + " - Spade"; break;
-						}
-						break;
-				}
-	
+				if(cardtype == 1) shape = "Diamond";	
+				else if(cardtype == 2) shape = "Clover";
+				else if(cardtype == 3) shape = "Heart";
+				else if(cardtype == 4) shape = "Spade";
+
+				if(cardnum == 1) cardDigit = "A";
+				else if(cardnum == 11) cardDigit = "Jack";
+				else if(cardnum == 12) cardDigit = "Queen";
+				else if(cardnum == 13) cardDigit = "King";
+				else cardDigit = Integer.toString(cardnum);
+					
+				card = cardDigit + " - " + shape;
 				unshuffledDeck.push(card);
 			}
 			
+		for(Iterator<String> iterator = unshuffledDeck.descendingIterator(); iterator.hasNext();) 
+			System.out.println(iterator.next());
+
 		while(unshuffledDeck.size() != 0) {
 			randpos = (int)(Math.random() * unshuffledDeck.size());
 			
 			deck.push(unshuffledDeck.get(randpos));
-			
 			unshuffledDeck.remove(randpos);
 		}
-
-		for(Iterator<String> iterator = unshuffledDeck.descendingIterator(); iterator.hasNext();) 
-			System.out.println(iterator.next());
 	}
 
-		
-
-	String drawCard() {
-		String draw = deck.peek();
-		deck.pop();
-		
-		return draw;
-	}
+	public String drawCard() {return deck.pop();}
 	
-	static Stack<String> deck;
+	private String card = new String();
+	private static Stack<String> deck = new Stack<String>();
 }
